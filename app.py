06b8fcfa4b681fc51ea8344dfc8f327bbafcf638
Rendy-py <farrelinorendy@gmail.com>
2026-09-24 10:34:53 +0700

    Refactor project details and update images
    Updated project images and fixed formatting issues.

--- app.py
+++ app.py
@@ -14,11 +14,11 @@ def projects():
     my_projects = [
         {"name": "Nusantara 7 AUV",
          "desc": "Led a team in the end-to-end development of an Autonomous Underwater Vehicle (AUV) from the ground up, overseeing system design, software development, hardware-software integration, testing, and deployment for the Singapore AUV Challenge (SAUVC). Developed closed-loop control systems using PID tuning to achieve stable and responsive vehicle motion, integrated computer vision for visual-based navigation, and utilized a CMPS12 digital compass for precise heading estimation and orientation control. Additionally, configured and integrated acoustic pinger systems to enable accurate underwater distance detection, obstacle avoidance, and enhanced spatial awareness during autonomous operations.",
-         "image": "asset/foto-formal-3x4 - Farrelino Rendy Prabowo (1) (1).png"},
+         "image": "asset/sauvc.jpeg"},
 
         {"name": "Pelamis 8 ASV",
          "desc": "Architected and developed the complete software framework for an autonomous surface vessel from scratch, encompassing system automation, thruster allocation, and programmatic vessel control. Developed and integrated data telemetry systems to enable real-time monitoring, diagnostics, and operational control, while implementing advanced computer vision capabilities for autonomous perception. Optimized vessel navigation and maneuverability through extensive PID tuning and control-system refinement, ensuring stable, precise, and reliable autonomous surface operations.",
-         "image": "https://via.placeholder.com/300x200?text=Project+Beta"},
+         "image": "asset/KKI.jpeg"},
 
         {"name": "YOLO Hand Tracking",
          "desc": "Servo etc...",
